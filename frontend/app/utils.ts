22127c4913f977aca6e1d5de6aa4560d2580d162
John <johnzhengyuliu@gmail.com>
2024-07-10 12:32:01 -0500

    frontend

---
+++ frontend/app/utils.ts
@@ -0,0 +1,52 @@
+import { PriceFeed } from "@pythnetwork/price-service-client";
+import { LAMPORTS_PER_SOL, PublicKey } from "@solana/web3.js";
+
+const PRICE_FEED_DECIMAL_ADJUSTMENT = 10;
+
+export function calculateHealthFactor(
+  lamportBalance: number,
+  amountMinted: number,
+  liquidationThreshold: number,
+  priceFeed: PriceFeed,
+): number {
+  const collateralValueInUsd = getUsdValue(lamportBalance, priceFeed);
+
+  const collateralAdjustedForLiquidationThreshold =
+    (collateralValueInUsd * liquidationThreshold) / 100;
+
+  if (amountMinted === 0) {
+    return Number.MAX_SAFE_INTEGER;
+  }
+
+  return collateralAdjustedForLiquidationThreshold / amountMinted;
+}
+
+export function getUsdValue(
+  amountInLamports: number,
+  priceFeed: PriceFeed,
+): number {
+  const price = priceFeed.getPriceNoOlderThan(60);
+  if (!price) {
+    throw new Error("Invalid price");
+  }
+
+  const priceInUsd = Number(price.price) * PRICE_FEED_DECIMAL_ADJUSTMENT;
+  const amountInUsd = (amountInLamports * priceInUsd) / LAMPORTS_PER_SOL;
+
+  return amountInUsd;
+}
+
+export function getLamportsFromUsd(
+  amountInUsd: number,
+  priceFeed: PriceFeed,
+): number {
+  const price = priceFeed.getPriceNoOlderThan(60);
+  if (!price) {
+    throw new Error("Invalid price");
+  }
+
+  const priceInUsd = Number(price.price) * PRICE_FEED_DECIMAL_ADJUSTMENT;
+  const amountInLamports = (amountInUsd * LAMPORTS_PER_SOL) / priceInUsd;
+
+  return amountInLamports;
+}
---
+++ frontend/components/site-header.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import Link from "next/link";
+
+import { siteConfig } from "@/config/site";
+import { MainNav } from "@/components/main-nav";
+import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
+// import { ThemeToggle } from "@/components/theme-toggle";
+
+export function SiteHeader() {
+  return (
+    <header className="sticky top-0 z-40 w-full border-b bg-background">
+      <div className="container flex h-16 items-center space-x-4 sm:justify-between sm:space-x-0">
+        <MainNav items={siteConfig.mainNav} />
+        <div className="flex flex-1 items-center justify-end space-x-4">
+          <nav className="flex items-center space-x-4">
+            <WalletMultiButton />
+          </nav>
+        </div>
+      </div>
+    </header>
+  );
+}
